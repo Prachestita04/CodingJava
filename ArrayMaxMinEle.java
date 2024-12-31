7f4b96980490533f967b82f00bfb0c0380c2255f
Prachestita04 <prachestitamahapatra@gmail.com>
2024-12-31 13:00:12 +0530

    Practice program 7 commit

---
+++ ArrayMaxMinEle.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ArrayMaxMinEle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array size:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter array elements");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        findMaxMin(arr);
+
+    }
+
+    private static void findMaxMin(int[] a) {
+        int min = a[0], max = a[0];
+        for (int j = 0; j < a.length; j++) {
+            if (min > a[j]) {
+                min = a[j];
+            }
+            if (max < a[j]) {
+                max = a[j];
+            }
+        }
+        System.out.println("The minimum element in an array is: " + min);
+        System.out.println("The maximum element in an array is:  " + max);
+    }
+}
---
+++ DuplicateElementArray.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+
+public class DuplicateElementArray {
+    public static void main(String[] args) {
+        String[] arr = {"Java" ,"C","Python","C","Html"};
+        //findDuplicate(arr);
+        usingHashSet(arr);
+    }
+    private static void findDuplicate(String[] a){
+        boolean flag=false;
+        for (int i=0;i< a.length;i++){
+            for (int j=i+1;j< a.length;j++){
+                if (a[i]==a[j]){
+                    System.out.println("Array contains duplicate element: "+a[i]);
+                    flag = true;
+                }
+            }
+        }
+        if(!flag){
+            System.out.println("Array does not contain any duplicate element ");
+        }
+    }
+    private static void usingHashSet(String[] b){
+        HashSet <String>langs = new HashSet();
+        boolean flag = false;
+//        System.out.println(langs.add("Java"));
+//        System.out.println(langs.add("Python"));
+//        System.out.println(langs.add("Java"));
+        for(String l : b){
+            if(langs.add(l)==false){
+                System.out.println("Array contains duplicate elements: "+l);
+                flag = true;
+            }
+        }
+        if (!flag){
+            System.out.println("Array does not contain duplicate element");
+        }
+
+    }
+}
